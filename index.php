5f268f876e4b4601f174568be3ae0fa3f86bb715
Wahyu Wardaya <wardaya56@gmail.com>
2024-05-02 04:05:09 +0800

    membuat file struktur html dan menginstall Bootstrap css, js, dan jquery

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Pakar Diagnosa Penyakit Kulit</title>
+
+    <!-- Bootstrap css -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+    
+<!-- jquery -->
+<link rel="stylesheet" href="assets/js/jquery-3.7.0.min.js">
+<!-- bootstrap js -->
+<link rel="stylesheet" href="assets/js/bootstrap.min.js">
+</body>
+</html>
